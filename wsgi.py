d5cccd88e192d59555d312ee8e36d42be45f2983
mbach04 <mbach04@gmail.com>
2019-01-25 10:04:33 -0500

    initial commit, add banner template

--- wsgi.py
+++ wsgi.py
@@ -1,49 +1,22 @@
 from flask import Flask
+from flask import render_template
 import os
 
 
 
-application = Flask(__name__)
+app = Flask(__name__)
 
 
-def get_banner_js():
+@app.route("/")
+def banner():
+    # default to highest class
+    # Other common values:
+    # SECRET: #FF0000
+    # CONFIDENTIAL: #286090
+    # UNCLASSIFIED: #5cb85c
     classification = os.environ.get('CLASSIFICATION', 'TOP SECRET')
     banner_color = os.environ.get('BANNER_COLOR', '#f0ad4e')
-    js = '''
-    (function() {
-      setTimeout(function () {
-
-        // Header
-        var banner = document.createElement("div");
-        banner.className = "classification-banner";
-        banner.innerHTML = "{0}";
-        banner.style = "display: block; background-color: {1}; color: #fff; font-weight: bold; text-align: center; text-shadow: 1px 1px 0 #444; margin-left: -2rem; position: absolute; width: 98.5vw; height: 16px"
-
-        var content = document.getElementsByClassName("navbar-pf-vertical")[0];
-        if (typeof(content) != 'undefined' && content != null)
-        {
-          content.insertBefore(banner, content.firstChild);
-        } else {
-          var body = document.body;
-          body.insertBefore(banner, body.firstChild);
-        }
-
-        // Footer
-        var bannerFooter = document.createElement("footer");
-        bannerFooter.className = "classification-banner-footer";
-        bannerFooter.innerHTML = {0};
-        bannerFooter.style = "display: block; background-color: {1}; color: #fff; font-weight: bold; text-align: center; text-shadow: 1px 1px 0 #444; position: absolute; width: 98.5vw; bottom: 0; z-index: 99999; height: 16px"
-        var body = document.body;
-        body.appendChild(bannerFooter);
-
-      }, 1000);
-    }());'''.format(classification, banner_color)
-    return js
-
-
-@application.route("/")
-def hello():
-    return get_banner_js()
+    return render_template('banner.html', banner_color=banner_color, classification=classification)
 
 if __name__ == "__main__":
-    application.run()
+    app.run()
