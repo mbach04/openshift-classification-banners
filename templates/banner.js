ab556d03035a315dd9f81de536b63b131b0c1694
Matt <mbach04@users.noreply.github.com>
2019-01-25 12:51:13 -0500

    make footer fixed

--- templates/banner.js
+++ templates/banner.js
@@ -15,7 +15,7 @@
       var bannerFooter = document.createElement("footer");
       bannerFooter.className = "classification-banner-footer";
       bannerFooter.innerHTML = "{{ classification }}";
-      bannerFooter.style = "display: block; background-color: {{ banner_color }}; color: #fff; font-weight: bold; text-align: center; text-shadow: 1px 1px 0 #444; position: absolute; width: 100%; bottom: 0; z-index: 99999; height: 16px"
+      bannerFooter.style = "display: block; background-color: {{ banner_color }}; color: #fff; font-weight: bold; text-align: center; text-shadow: 1px 1px 0 #444; position: fixed; width: 100%; bottom: 0; z-index: 99999; height: 16px"
       var body = document.body;
       body.appendChild(bannerFooter);
     }, 1000);
